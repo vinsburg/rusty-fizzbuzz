81b348c34f7db4580f7b6b505231dda0c1c304dd
vinsburg <vinsburg@gmail.com>
2023-01-24 21:50:06 +0200

    Added test for "2"

--- tests/renderer_tests.rs
+++ tests/renderer_tests.rs
@@ -4,3 +4,8 @@ use fizzbuzz::renderer;
 pub fn test_should_return_one_as_string_for_one() {
     assert!("1" == renderer::render(1))
 }
+
+#[test]
+pub fn test_should_return_two_as_string_for_two() {
+    assert!("2" == renderer::render(2))
+}
