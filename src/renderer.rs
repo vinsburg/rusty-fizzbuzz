e15d85b3fbada0a9ec324ea4a7084ef79d212458
vinsburg <vinsburg@gmail.com>
2023-01-24 22:54:32 +0200

    Renders Buzz! for 5

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,7 +1,10 @@
 pub fn render(number: u32) -> String {
     let mut rendered: String = "".to_string();
-    if (number % 3) != 0 {
+    if (number % 3) == 0 {
         rendered = "Fizz".to_string();
+    }
+    if (number % 5) == 0 {
+        rendered.push_str("Buzz!");
     }
     if rendered == "" {
         rendered = number.to_string();
